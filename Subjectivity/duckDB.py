e598982482ecf02b0c292e21f7cd9311ede1fe73
Refija Oflaz <127984185+oflzrfa@users.noreply.github.com>
2025-01-24 15:13:34 +0100

    Update duckDB.py

--- Subjectivity/duckDB.py
+++ Subjectivity/duckDB.py
@@ -1,15 +1,10 @@
 import duckdb
 
-# Connect to the DuckDB database
 conn = duckdb.connect('my_database.duckdb')
 
-# Query to list all tables
 tables = conn.execute("SHOW TABLES").fetchdf()
 
-# Display the list of tables
 print(tables)
-
-# Close the connection
 conn.close()
 
 
